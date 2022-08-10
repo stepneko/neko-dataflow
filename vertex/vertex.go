6b56529f3dd3c83bc4680324fbd472d64af4deaf
vodkaslime <646329483@qq.com>
2022-08-10 09:14:36 +0800

    chnage const int variables to use iota

--- vertex/vertex.go
+++ vertex/vertex.go
@@ -11,14 +11,14 @@ const Id_Nil = 0
 type Type int
 
 const (
-	Type_Input          Type = 1
-	Type_Ingress        Type = 2
-	Type_IngressAdapter Type = 3
-	Type_Egress         Type = 4
-	Type_EgressAdapter  Type = 5
-	Type_Feedback       Type = 6
-	Type_Inspect        Type = 7
-	Type_Bianry         Type = 8
+	Type_Input Type = iota
+	Type_Ingress
+	Type_IngressAdapter
+	Type_Egress
+	Type_EgressAdapter
+	Type_Feedback
+	Type_Inspect
+	Type_Bianry
 )
 
 // Vertex is the interface that represents a vertex in the computing graph.
